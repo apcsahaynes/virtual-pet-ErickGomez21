8a28841fc467f111ad7e76c3c75db908089d312f
ErickGomez21 <erick.gomez@jpschools.org>
2026-03-06 11:06:20 -0600

    Add files via upload

---
+++ Liposuction.java
@@ -0,0 +1,17 @@
+public class Liposuction{
+ private String name;
+ private int originalWeight;
+ 
+ public Liposuction(String name, int originalWeight){
+   this.name = name;
+   this.originalWeight = originalWeight;
+ }
+ 
+ public String getName(){
+   return name;
+ }
+ 
+ public int getOriginalWeight(){
+  return originalWeight; 
+ }
+}
---
+++ VirtualPet.java
@@ -0,0 +1,87 @@
+public class VirtualPet {
+    private String name;
+    private int energy;
+    private int weight;
+    private int happiness;
+    private int ageMonths;
+    private int ageYears;
+    
+    
+    public VirtualPet(String name) {
+      this.name = name;
+      this.energy = 0;
+      this.weight = 5;
+      this.happiness = 0;
+      this.ageMonths = 0;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String toString() {
+        return name + "'s information: \nEnergy: " + energy + "\nWeight: " + weight + "\nHappiness: "+ happiness + "\nAge: " + ageYears + " years and " + ageMonths + " months";
+    }
+    
+    public int getEnergyLevel() {
+        return energy;
+    }
+    
+    public int getHappinessLevel() {
+        return happiness;
+    }
+    
+    public int getWeight(){
+     return weight;   
+    }
+    
+    public int getAgeYears(){
+     return ageYears; 
+    }
+    
+    public void feed(Food food) {
+        this.energy += food.getEnergyIncrease();
+        this.weight += food.getWeightGain(); 
+        this.happiness += food.getHappinessIncrease();
+    }
+    
+    public void play(Game game) {
+        if (game.isWinner()) {
+            this.happiness += game.getHappinessIncr();
+            this.weight -= game.getWeightDecr();
+        }
+    }
+    
+    public void whipHim(Whip whip){
+   if (getWeight() > 150){
+     this.weight -= whip.getWeightDecr();
+     this.happiness -= whip.getHappinessDecr();
+   }
+  }
+  
+  public void Lipo(Liposuction liposuction){
+    if (getWeight() > 150){
+      this.weight = liposuction.getOriginalWeight();
+    }
+  }
+    
+    public void updateStatus() {
+        if (happiness > 0) {
+            happiness--;
+        }
+        
+        if (energy > 0) {
+            energy--;
+        }
+        
+        ageMonths++;
+        ageConverter();
+    }
+    
+    private void ageConverter() {
+        if (ageMonths >= 12) {
+            ageYears++;
+            ageMonths = 0;
+        }
+    }
+}
